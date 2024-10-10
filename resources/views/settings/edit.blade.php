5a6e6efca55efb597ff052f46a8faea1557941fc
saurabhjadhav29 <saurabhjadhav29.di@gmail.com>
2024-10-10 12:23:53 +0530

    message change

--- resources/views/settings/edit.blade.php
+++ resources/views/settings/edit.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 @section('title')
-    {{ __('messages.settings') }}
+    {{-- {{ __('messages.settings') }} --}}
+    @section('title')
+    @if ($sectionName === 'general')
+        {{ __('General') }}
+    @elseif ($sectionName === 'terms-conditions')
+        {{ __('Terms-conditions') }}
+    @elseif ($sectionName === 'google_analytics')
+        {{ __('Google configuration') }}
+    @elseif ($sectionName === 'payment_method')
+        {{ __('Payment configuration') }}
+    @else
+        {{ __('messages.settings') }}
+    @endif
 @endsection
+
+@endsection
+
 @section('content')
     <div class="container-fluid">
         <div class="">
